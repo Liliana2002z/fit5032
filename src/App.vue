75d71765bf8c98befe20ada1e90510de7c00e445
Liliana2002z <xzha0522@student.monash.edu>
2025-08-19 22:41:24 +1000

    week3 done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
-<template>
-  <div id="app">
-    <JSONLab />
-  </div>
-</template>
-
-<script>
-import JSONLab from './components/JSON.vue'
-
-export default {
-  name: 'App',
-  components: {
-    JSONLab
-  }
-}
+<script setup>
+// import JSON from "./components/JSON.vue";text-align: center
+// import JSONSolution from "./components/JSONSolution.vue";
+import Form from "./components/UserForm.vue";
 </script>
+
+<template>
+  <Form />
+</template>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
   margin-top: 60px;
 }
